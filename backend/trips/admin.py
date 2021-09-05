eeb84d52ad6fa01c63843800a7a3e8ddd86d29b1
garyhung0584 <garyhung0584@gmail.com>
2021-09-05 15:02:40 +0800

    新增

---
+++ backend/trips/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Post, Author, Genre, Book, BookInstance
+
+
+admin.site.register(Post)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+admin.site.register(Author, AuthorAdmin)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
