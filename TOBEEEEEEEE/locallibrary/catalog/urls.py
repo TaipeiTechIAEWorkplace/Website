1d224eca21a7b4d73270917f151084a78db851d5
ChenWei <jamie44566@gmail.com>
2021-10-01 21:24:26 +0800

    表單

--- TOBEEEEEEEE/locallibrary/catalog/urls.py
+++ TOBEEEEEEEE/locallibrary/catalog/urls.py
@@ -10,3 +10,7 @@ urlpatterns = [
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
