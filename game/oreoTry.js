7b79479a7e933481ec4e42be067887d17a6d9e80
irene-tiamo <87821949+irene-tiamo@users.noreply.github.com>
2021-10-11 20:02:41 +0800

    trying oreo
    做到一半

--- game/oreoTry.js
+++ game/oreoTry.js
@@ -7,11 +7,14 @@ function isInsideButton(pos,rect){
     return pos.x >rect.x && pos.x < rect.x + rect.width && pos.y < rect.y + rect.height && pos.y > rect.y
 }
 
-function drawButton(name, color, text){
+function drawButton(name, color, text, textY){
     ctx.fillStyle = color;
     ctx.fillRect(name.x, name.y, name.width, name.height);
+    ctx.font="bold 30px sans-serif";
     ctx.fillStyle="black";
-    ctx.fillText(text, 0, 0);
+    ctx.textBaseline="middle";
+    ctx.textAlign="center";
+    ctx.fillText(text, canvas.width*0.85, textY);
 }
 
 function getMousePos(canvas, event){
@@ -27,48 +30,122 @@ function question(text){
     this.y=canvas.height*0.8;
     this.width=canvas.width*0.5;
     this.height=canvas.height*0.2;
-    this.text=text;
    
     this.draw=function(){
       ctx.strokeRect(this.x, this.y, this.width, this.height);
-      ctx.font="20px";
+      ctx.font="bold 28px sans-serif";
       ctx.fillStyle="black";
-      ctx.fillText(this.text, 0, 0);
+      ctx.textBaseline="middle";
+      ctx.textAlign="center";
+      ctx.fillText(text, canvas.width*0.35, canvas.height*0.9);
     }
     this.draw();
 }
 
-function result(text){
-    this.x=canvas.width*0.1;
-    this.y=canvas.height*0.8;
-    width=canvas.width*0.5;
-    height=canvas.height*0.2;
-    ctx.strokeRect(x, y, width, height);
-    ctx.font="20px";
-    ctx.fillStyle="black";
-    ctx.fillText(text, 0, 0);
+function result(text, choice){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    question(text);
+    if (choice == 'true'){
+      drawButton(next, next.color, next.text, next.textY);
+    }   
+    else{
+      drawButton(again, again.color, again.text, again.textY);
+    }
 }
 
 canvas.addEventListener('click', function(evt){
     var mousePos = getMousePos(canvas, evt);
         if (isInsideButton(mousePos, button1)){
             console.log("hi");
-            if (button1.choice == "false"){
-              result("教室還沒開門，走廊的燈也壞掉了，被隱身的tobe嚇了一跳")
-          }
+            result(button1.result, button1.choice)
         }
 });
 
+canvas.addEventListener('click', function(evt){
+  var mousePos = getMousePos(canvas, evt);
+      if (isInsideButton(mousePos, button2)){
+          console.log("hi");
+          result(button2.result, button2.choice)
+      }
+});
+
+canvas.addEventListener('click', function(evt){
+  var mousePos = getMousePos(canvas, evt);
+      if (isInsideButton(mousePos, button3)){
+          console.log("hi");
+          result(button3.result, button3.choice)
+      }
+});
+
+canvas.addEventListener('click', function(evt){
+  var mousePos = getMousePos(canvas, evt);
+      if (isInsideButton(mousePos, again)){
+          console.log("hi");
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          question("今天要什麼時候到教室，你決定?");
+          drawButton(button1, button1.color, button1.text, button1.textY);
+          drawButton(button2, button2.color, button2.text, button2.textY);
+          drawButton(button3, button3.color, button3.text, button3.textY);
+      }
+});
 
 var button1 = {
     x: canvas.width*0.7,
-    y: 180,
+    y: canvas.height*0.05,
     width: canvas.width*0.3,
-    height: 50,
+    height: canvas.height*0.25,
+    choice:'false',
     color: '#EFE7DC',
-    choice: "false",
+    result: "教室還沒開門，走廊的燈也壞掉了，被隱身的tobe嚇了一跳",
     text: "提前二十分鐘到",
+    textY: canvas.height*0.175,
 };
 
-question("今天要什麼時候到教室，你決定");
-drawButton(button1, button1.color, button1.text);
+var button2={
+    x: canvas.width*0.7,
+    y: canvas.height*0.35,
+    width: canvas.width*0.3,
+    height: canvas.height*0.25,
+    choice:'true',
+    color: '#EFE7DC',
+    result: "在摩斯漢堡等時間差不多再走，享受早上的寧靜時光",
+    text: "準時壓線",
+    textY: canvas.height*0.475,
+}
+
+var button3={
+  x: canvas.width*0.7,
+  y: canvas.height*0.65,
+  width: canvas.width*0.3,
+  height: canvas.height*0.25,
+  choice:'true',
+  color: '#EFE7DC',
+  result: "你坐在最後一排，老師今天沒點名，你逃過一劫",
+  text: "第二節才來",
+  textY: canvas.height*0.775,
+}
+
+var next={
+    x: canvas.width*0.7,
+    y: canvas.height*0.375,
+    width: canvas.width*0.3,
+    height: canvas.height*0.25,
+    color: '#EFE7DC',
+    text: "下一關",
+    textY: canvas.height*0.5,
+}
+
+var again={
+  x: canvas.width*0.7,
+  y: canvas.height*0.375,
+  width: canvas.width*0.3,
+  height: canvas.height*0.25,
+  color: '#EFE7DC',
+  text: "再來一次",
+  textY: canvas.height*0.5,
+}
+
+question("今天要什麼時候到教室，你決定?");
+drawButton(button1, button1.color, button1.text, button1.textY);
+drawButton(button2, button2.color, button2.text, button2.textY);
+drawButton(button3, button3.color, button3.text, button3.textY);
